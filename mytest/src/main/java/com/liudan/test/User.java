35dff79cfcb621cf39e5fdb7d4b4833a95d0684e
liudan <xiao@123.cn>
2018-10-28 11:33:02 +0800

    qq

---
+++ mytest/src/main/java/com/liudan/test/User.java
@@ -0,0 +1,15 @@
+package com.liudan.test;
+
+/**
+ * @ClassName User
+ * @Description TODO
+ * @Author xiaod
+ * @Dtate 2018/10/28 11:17
+ * @Version 1.0
+ **/
+public class User {
+
+    public static void main(String[] args) {
+        System.out.println("______________________1111_______________________");
+    }
+}
